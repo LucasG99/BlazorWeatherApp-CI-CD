f1eb49e3897193542544b7eec02fc8e1fd812cd8
LucasG99 <lucas.galvao@sou.inteli.edu.br>
2025-03-27 09:29:56 -0300

    Commit 2: Implementação do controller de Weather e configuração da API

---
+++ BlazorWeatherApp.Api/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather API", Version = "v1" });
+});
+
+// Adicionar CORS para permitir chamadas do cliente Blazor
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowBlazorClient");
+app.MapControllers();
+
+app.Run();
